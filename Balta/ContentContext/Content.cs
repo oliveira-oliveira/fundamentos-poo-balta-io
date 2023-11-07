67fdeb4118750552813e0b3644b819ca9166186f
Diego Oliveira <diego.oliveira@grupocheck.com.br>
2023-11-07 14:28:08 -0300

    Iniciando contexto de conteúdo

---
+++ Balta/ContentContext/Content.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Balta.ContentContext
+{
+    public class Content
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
